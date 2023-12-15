6fe7bb7800323b9c83f17d4461246d70babc3025
Kamil Lepkowski <lepkowskikamil@yahoo.com>
2023-12-15 01:30:14 -0600

    Rework favorite locations screen into grid view Add new random color function Add current location info to drawer header

--- lib/screens/favorite_locations.dart
+++ lib/screens/favorite_locations.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_app/screens/weather_app.dart';
 
@@ -23,41 +25,98 @@ class FavoriteLocationsScreenState extends State<FavoriteLocationsScreen> {
     });
   }
 
+  Color _getRandomColor(String cityName) {
+    final random = Random(cityName.hashCode);
+    return Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+    );
+  }
+
+  String capitalizeEveryFirstLetter(String input) {
+    List<String> words = input.split(' ');
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].isNotEmpty) {
+        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+      }
+    }
+    return words.join(' ');
+  }
+
+  void _removeCard(int index) {
+    setState(() {
+      favoriteLocations.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 15, 21, 34),
       appBar: AppBar(
-        title: const Text('Favorite Locations'),
+        title: const Text(
+          'Favorite Locations',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
       ),
-      body: ListView.builder(
-        itemCount: favoriteLocations.length,
-        itemBuilder: (context, index) {
-          final location = favoriteLocations[index];
-          return ListTile(
-            title: Row(
-              children: [
-                Expanded(
-                  child: Text(location),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // You can adjust the cross-axis count as needed
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+          ),
+          itemCount: favoriteLocations.length,
+          itemBuilder: (context, index) {
+            String location = favoriteLocations[index];
+            location = capitalizeEveryFirstLetter(location);
+            return Card(
+              color: Colors.white.withOpacity(0.05),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WeatherApp(city: location),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          location,
+                          style: TextStyle(
+                              color: _getRandomColor(location),
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      InkWell(
+                        child: const Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                        onTap: () async {
+                          _removeCard(index);
+                          FavoriteLocations.removeFavoriteLocation(location);
+                        },
+                      )
+                    ],
+                  ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () async {
-                    await FavoriteLocations.removeFavoriteLocation(location);
-                    _loadFavoriteLocations();
-                  },
-                ),
-              ],
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => WeatherApp(city: location),
-                ),
-              );
-            },
-          );
-        },
+              ),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
